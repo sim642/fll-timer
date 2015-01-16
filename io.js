6058541440065d81daa739af07c96bed1fc16c18
Simmo Saan <simmo.saan@gmail.com>
2015-01-16 08:03:40 +0200

    Table view allows for selecting which tables it shows

--- io.js
+++ io.js
@@ -46,7 +46,7 @@ io.on('connection', function(socket) {
 	console.log('client connected');
 	socket.emit('teams', teams);
 	socket.emit('rounds', rounds);
-	socket.emit('showteams', [1, 3]);
+	socket.emit('showteams', [0, 1, 2, 3]);
 
 	socket.on('disconnect', function() {
 		console.log('client disconnected');
